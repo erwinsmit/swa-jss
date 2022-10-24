ac9d57ff7d95fefa6b52bbed041e9c6cdb6b6a3a
Erwin Smit <erwin.smit@gmail.com>
2022-10-24 12:34:47 +0200

    initial commit

---
+++ src/lib/page-props.ts
@@ -0,0 +1,18 @@
+import { Redirect } from 'next';
+import {
+  DictionaryPhrases,
+  ComponentPropsCollection,
+  LayoutServiceData,
+} from '@sitecore-jss/sitecore-jss-nextjs';
+
+/**
+ * Sitecore page props
+ */
+export type SitecorePageProps = {
+  locale: string;
+  dictionary: DictionaryPhrases;
+  componentProps: ComponentPropsCollection;
+  notFound: boolean;
+  layoutData: LayoutServiceData;
+  redirect?: Redirect;
+};
