7a061b952baf75e9a122c41e077070cf018e8a05
Erwin Smit <erwin.smit@gmail.com>
2022-10-24 21:42:21 +0200

    log username stuff

--- log-stuff.js
+++ log-stuff.js
@@ -1,5 +1,11 @@
 // log out environment variables
 console.log(require('dotenv').config());
 
+var username = require('username-sync');
+console.log("username: " + username);
+// log out username
+console.log(username());
+
+
 console.log('process.env.SITECORE_API_KEY', process.env.SITECORE_API_KEY);
 console.log('process.env.SITECORE_API_HOST', process.env.SITECORE_API_HOST);
