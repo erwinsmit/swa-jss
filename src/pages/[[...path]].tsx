a703e568c2f9422cf0728daec7861c79c3a56e13
Erwin Smit <erwin.smit@gmail.com>
2022-10-24 12:59:15 +0200

    adding ISR

--- src/pages/[[...path]].tsx
+++ src/pages/[[...path]].tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { GetServerSideProps } from 'next';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import NotFound from 'src/NotFound';
 import Layout from 'src/Layout';
 import {
@@ -49,19 +49,45 @@ const SitecorePage = ({ notFound, componentProps, layoutData }: SitecorePageProp
   );
 };
 
-// This function gets called at request time on server-side.
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const props = await sitecorePagePropsFactory.create(context);
+// This function gets called at build and export time to determine
+// pages for SSG ("paths", as tokenized array).
+export const getStaticPaths: GetStaticPaths = async () => {
+  // Fallback, along with revalidate in getStaticProps (below),
+  // enables Incremental Static Regeneration. This allows us to
+  // leave certain (or all) paths empty if desired and static pages
+  // will be generated on request (development mode in this example).
+  // Alternatively, the entire sitemap could be pre-rendered
+  // ahead of time (non-development mode in this example).
+  // See https://nextjs.org/docs/basic-features/data-fetching#incremental-static-regeneration
 
-  // Check if we have a redirect (e.g. custom error page)
-  if (props.redirect) {
+  if (process.env.NODE_ENV !== 'development') {
+    // Note: Next.js runs export in production mode
+    // const paths = await sitemapFetcher.fetch(context);
+
     return {
-      redirect: props.redirect,
+      paths: [],
+      fallback: process.env.EXPORT_MODE ? false : 'blocking',
     };
   }
 
   return {
+    paths: [],
+    fallback: true,
+  };
+};
+
+// This function gets called at build time on server-side.
+// It may be called again, on a serverless function, if
+// revalidation (or fallback) is enabled and a new request comes in.
+export const getStaticProps: GetStaticProps = async (context) => {
+  const props = await sitecorePagePropsFactory.create(context);
+
+  return {
     props,
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 5 seconds
+    revalidate: 5, // In seconds
     notFound: props.notFound, // Returns custom 404 page with a status code of 404 when true
   };
 };
