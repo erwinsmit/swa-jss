2f28c587f23a8822ad66a129fba3dc95e8ac8548
Erwin Smit <erwin.smit@gmail.com>
2022-10-24 21:13:40 +0200

    log more stuff

---
+++ log-stuff.js
@@ -0,0 +1,5 @@
+// log out environment variables
+console.log(require('dotenv').config());
+
+console.log('process.env.SITECORE_API_KEY', process.env.SITECORE_API_KEY);
+console.log('process.env.SITECORE_API_HOST', process.env.SITECORE_API_HOST);
